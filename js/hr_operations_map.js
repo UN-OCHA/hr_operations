aaea4425d3809e43c7fa8c9c9718e45ad577aae2
maximeg01 <maxime.gaul@feelandclic.com>
2014-12-04 14:56:37 +0100

    Pager loop JS

--- js/hr_operations_map.js
+++ js/hr_operations_map.js
@@ -1,55 +1,70 @@
 (function($) {
   $(document).ready(function() {
     // Get Data
-    $.getJSON(window.location.protocol + '//' + window.location.host + '/api/v1.0/operations?filter[type]=country&filter[status]=active', function(data) {
-        $(data.data).each(function(i) {
-            // Set country code
-            if(data.data[i].country != null) {
-                data.data[i].mapCode = data.data[i].country.pcode;
+    $.getJSON(window.location.protocol + '//' + window.location.host + '/api/v1.0/operations?filter[type]=country', function(data) {
+
+      var countriesMap = data.data;
+
+      // Pager loop
+      while(data.next) {
+        delete data; // Reset data
+        $.ajax({
+          url: data.next.href,
+          async:false
+        })
+        .done(function(dataPager) {
+          data = dataPager;
+          countriesMap = countriesMap.concat(data.data);
+        });
+      }
+
+      $(countriesMap).each(function(i) {
+        // Set country code
+        if(countriesMap[i].country != null) {
+          countriesMap[i].mapCode = countriesMap[i].country.pcode;
+        }
+        //Format date
+        var launchDate = new Date(countriesMap[i].launch_date*1000);
+        countriesMap[i].launch_date = launchDate.getDate() + "/" + (1+launchDate.getMonth()) + "/" + launchDate.getFullYear();
+      });
+
+      // Initiate the chart
+      $('#operations_map').highcharts('Map', {
+        colors: ['#BC2E3B'],
+        chart : {
+          backgroundColor : '#E0ECED'
+        },
+        title : {
+          text : ''
+        },
+        legend: {
+          enabled: false
+        },
+        plotOptions: {
+          map: {
+            joinBy: ['iso-a2', 'mapCode'],
+            dataLabels: {
+              enabled: true,
+              format: '{point.label}'
+            },
+            mapData: Highcharts.maps['custom/world'],
+            tooltip: {
+              headerFormat: '',
+              pointFormat: '<b>{point.name}</b><br/>{point.email}<br/>Launched: {point.launch_date}'
             }
-            //Format date
-            var launchDate = new Date(data.data[i].launch_date*1000);
-            console.log(launchDate);
-            data.data[i].launch_date = launchDate.getDate() + "/" + (1+launchDate.getMonth()) + "/" + launchDate.getFullYear();
-        });
 
-        // Initiate the chart
-        $('#operations_map').highcharts('Map', {
-            colors: ['#BC2E3B'],
-            chart : {
-                backgroundColor : '#E0ECED'
-            },
-            title : {
-                text : ''
-            },
-            legend: {
-                enabled: false
-            },
-            plotOptions: {
-                map: {
-                    joinBy: ['iso-a2', 'mapCode'],
-                    dataLabels: {
-                        enabled: true,
-                        format: '{point.label}'
-                    },
-                    mapData: Highcharts.maps['custom/world'],
-                    tooltip: {
-                        headerFormat: '',
-                        pointFormat: '<b>{point.name}</b><br/>{point.email}<br/>Launched: {point.launch_date}'
-                    }
-
-                }
-            },
-            series : [{
-                data : data.data, //empty
-                name: ' ',
-                states: {
-                    hover: {
-                        color: '#DD5763'
-                    }
-                }
-            }]
-        });
+          }
+        },
+        series : [{
+          data : countriesMap,
+          name: ' ',
+          states: {
+            hover: {
+              color: '#DD5763'
+            }
+          }
+        }]
+      });
     });
   });
 })(jQuery);
