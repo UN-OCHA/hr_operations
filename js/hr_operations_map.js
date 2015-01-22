29babb3b3758779798916cf5250438fab5d1a74f
Guillaume Viguier <guillaume@viguierjust.com>
2015-01-22 10:05:00 +0100

    Improved operations map

--- js/hr_operations_map.js
+++ js/hr_operations_map.js
@@ -1,7 +1,7 @@
 (function($) {
   $(document).ready(function() {
     // Get Data
-    $.getJSON(window.location.protocol + '//' + window.location.host + '/api/v1.0/operations?filter[type]=country', function(data) {
+    $.getJSON(window.location.protocol + '//' + window.location.host + '/api/v1.0/operations?filter[type]=country&filter[status]=active', function(data) {
 
       var countriesMap = data.data;
 
@@ -24,13 +24,19 @@
           countriesMap[i].mapCode = countriesMap[i].country.pcode;
         }
         //Format date
-        var launchDate = new Date(countriesMap[i].launch_date*1000);
-        countriesMap[i].launch_date = launchDate.getDate() + "/" + (1+launchDate.getMonth()) + "/" + launchDate.getFullYear();
+        if (countriesMap[i].launch_date != null) {
+          var launchDate = new Date(countriesMap[i].launch_date*1000);
+          countriesMap[i].launch_date = "Launched: " + launchDate.getDate() + "/" + (1+launchDate.getMonth()) + "/" + launchDate.getFullYear();
+        }
 
         //Click on country
         countriesMap[i].events = {
             click: function(e){
-                window.location.href = countriesMap[i].homepage;
+                var url = countriesMap[i].url;
+                if (countriesMap[i].homepage != null) {
+                  url = countriesMap[i].url;
+                }
+                window.location.href = url;
             }
         };
       });
@@ -51,14 +57,10 @@
         plotOptions: {
           map: {
             joinBy: ['iso-a2', 'mapCode'],
-            dataLabels: {
-              enabled: true,
-              format: '{point.label}'
-            },
             mapData: Highcharts.maps['custom/world'],
             tooltip: {
               headerFormat: '',
-              pointFormat: '<b>{point.name}</b><br/>{point.email}<br/>Launched: {point.launch_date}'
+              pointFormat: '<b>{point.name}</b><br/>{point.email}<br/>{point.launch_date}'
             }
 
           }
